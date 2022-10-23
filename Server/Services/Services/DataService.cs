90e98752de892849d0f647a93f9a8735c872d0f1
Paulo <paulocesarhero@gmail.com>
2022-10-23 12:31:43 -0500

    Servidor ejecutandose desde la linea de comando endPoint definito en appConfig del proyecto Host

---
+++ Server/Services/Services/DataService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Services
+{
+    public class DataService : IDataService
+    {
+        public bool AddCredentials(DTOCredentials credentials)
+        {
+            Console.WriteLine("Se agrego la credencial " + credentials.Username);
+            return true;
+        }
+
+        public bool AddImages()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
